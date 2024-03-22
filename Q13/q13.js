72e9763251fda0c99e43bec8973b330c3ae38461
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:25:28 +0500

    Exercise#13

---
+++ Q13/q13.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let transportation = ["Revo", "Rolls-Royce", "W11-Bus", "Bike"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log("i would like to own a =" + transportation[i]);
+}
---
+++ Q13/q13.ts
@@ -0,0 +1,6 @@
+let  transportation: string[] = ["Revo", "Rolls-Royce", "W11-Bus" , "Bike"];
+
+for(let i=0 ; i<transportation.length ; i++){
+    console.log("i would like to own a =" + transportation[i]);
+    
+}
