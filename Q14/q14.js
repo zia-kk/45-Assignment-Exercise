03f6e5749309ec14d673a136b0cd950b64827cff
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:26:34 +0500

    Exercise#14

---
+++ Q14/q14.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let myguests_List = [
+    "Sir Zia Khan",
+    "Sir Ameen Alam",
+    "Sir Mobashir Ali",
+    "Sir Usama",
+];
+for (let i = 0; i < myguests_List.length; i++) {
+    console.log(`Dear Respected Sir!:` +
+        myguests_List[i] +
+        `,\n we invited you on a Dinner.\n\n Thank You`);
+}
