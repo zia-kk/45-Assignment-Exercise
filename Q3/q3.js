a501e4d541a1e8113787ec0e1b9a4e75f300a41d
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:00:19 +0500

    Exercise #3

---
+++ Q3/q3.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fName = "Zia ur rehman";
+////LowerCase////
+console.log("LowerCase=", fName.toLowerCase());
+////UpperCase////
+console.log("UpperCase=", fName.toUpperCase());
+////TitleCase////
+console.log("TitleCase=", fName.charAt(0).toUpperCase() + fName.slice(1).toLowerCase());
---
+++ Q3/q3.ts
@@ -0,0 +1,10 @@
+let fName:string = "Zia ur rehman";
+
+////LowerCase////
+console.log("LowerCase=",fName.toLowerCase());
+
+////UpperCase////
+console.log("UpperCase=",fName.toUpperCase());
+
+////TitleCase////
+console.log("TitleCase=",fName.charAt(0).toUpperCase() + fName.slice(1).toLowerCase());
