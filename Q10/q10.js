bbb9d60aa1dbb873018ffbfe807170bc7efc9f11
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:18:08 +0500

    Exercise#10

---
+++ Q10/q10.js
@@ -0,0 +1,7 @@
+"use strict";
+// Day#4 chellenge 10
+// auther name: Zia
+// Dated: 10/04/2024
+Object.defineProperty(exports, "__esModule", { value: true });
+//simple programme of "Hello;world!"
+console.log('Hello,World! this is a simple program');
---
+++ Q10/q10.ts
@@ -0,0 +1,7 @@
+// Day#4 chellenge 10
+// auther name: Zia
+// Dated: 10/04/2024
+
+//simple programme of "Hello;world!"
+
+console.log('Hello,World! this is a simple program');
