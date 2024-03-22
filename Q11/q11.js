43f5b2a106738134655997e5faa9c2696339a179
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:22:29 +0500

    Exercise#11

---
+++ Q11/q11.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let members = ['imran', 'anas', 'zia', 'ali', 'umar'];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ Q11/q11.ts
@@ -0,0 +1,6 @@
+let members:string[]=['imran','anas','zia','ali','umar'];
+
+for(let i=0 ; i<members.length ; i++){
+    console.log(members[i]);
+    
+}
