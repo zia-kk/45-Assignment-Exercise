2b436768f7705718545890843d825321ed7fde29
Zia Ur Rehman <pkzia112@gmail.com>
2024-03-22 21:01:49 +0500

    Exercise #4

---
+++ Q4/q4.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let qoute = "“A person who never made a mistake never tried anything new.”";
+let auther = "Albert Einstein";
+console.log(`${auther} once said, ${qoute}`);
---
+++ Q4/q4.ts
@@ -0,0 +1,4 @@
+let qoute:string="“A person who never made a mistake never tried anything new.”";
+let auther:string="Albert Einstein";
+
+console.log(`${auther} once said, ${qoute}`);
