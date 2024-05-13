bf0d27c7afa6dcdcbde26c37f07495fd4edab45b
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-13 21:46:07 +0500

    Exercise 19

---
+++ Q19/q19.js
@@ -0,0 +1,7 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+const q14_1 = require("../Q14/q14");
+console.log(`\n PRINTING NUMBER OF INVITED GUEST`);
+console.log(`We Had Finaly Invited : ${q14_1.myguests_List.length} (Data collect from question 14)`);
---
+++ Q19/q19.ts
@@ -0,0 +1,9 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+// print a message indicating the number of people you are inviting to dinner.
+
+import { myguests_List } from"../Q14/q14"
+
+console.log(`\n PRINTING NUMBER OF INVITED GUEST`);
+console.log(`We Had Finaly Invited : ${myguests_List.length} (Data collect from question 14)`);
+
+ 
