90c2d22fbf6bc2cb042dfd54990d8de5e01df391
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-19 09:57:19 +0500

    Exercise 22

---
+++ Q22/q22.js
@@ -0,0 +1,11 @@
+"use strict";
+// undefined Error
+// let array:(string|number)[]=["zia","shehzad" , 1,3,5]
+// console.log(array[5]);
+Object.defineProperty(exports, "__esModule", { value: true });
+//Error///
+// let array:(string|number)[]=["zia","shehzad" , 1,3,5,]
+// console.log(array[5]);
+// Finally Solve Error
+let array = ["zia", "shehzad", 1, 3, 5, "fahad"];
+console.log(array[5]);
---
+++ Q22/q22.ts
@@ -0,0 +1,17 @@
+
+
+// undefined Error
+// let array:(string|number)[]=["zia","shehzad" , 1,3,5]
+// console.log(array[5]);
+
+//Error///
+
+// let array:(string|number)[]=["zia","shehzad" , 1,3,5,]
+// console.log(array[5]);
+
+// Finally Solve Error
+
+let array:(string|number)[]=["zia","shehzad" , 1,3,5,"fahad"]
+console.log(array[5]);
+
+
