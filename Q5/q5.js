2a23c7e1c82cdb1500c21861df4bb1715d2a4213
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-19 12:07:45 +0500

    removed

--- Q5/q5.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let qoute = "“A person who never made a mistake never tried anything new.”";
-let famous_Person = "Albert Einstein";
-let message = `${famous_Person} once said, ${qoute}`;
-console.log(message);
