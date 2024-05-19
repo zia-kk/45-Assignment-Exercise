5f9986c1b4a79a1ec3d33251045830fd7f51ac0e
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-19 12:08:56 +0500

    Exercise Q23

---
+++ Q23/README.md
@@ -0,0 +1,5 @@
+23.	Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
---
+++ Q23/q23.ts
@@ -0,0 +1,54 @@
+let car:string= "Cultus"
+// //Test Condition #1 /////
+
+console.log("Is car == 'Cultus'? I predict True.")
+console.log(car == 'Cultus')
+
+// //Test Condition #2/////
+
+console.log("Is car === 'Cultus'? I predict True.")
+console.log(car === 'Cultus')
+
+// //Test Condition #3/////
+
+console.log("Is car != 'Cultus'? I predict True.")
+console.log(car != 'Cultus')
+
+// ///Test Condition #4///
+
+console.log("Is car !== 'Cultus'? I predict True.")
+console.log(car !== 'Cultus')
+
+// ///Test Condition #5 ///
+
+console.log("Is car < 'Cultus'? I predict True.")
+console.log(car < 'Cultus')
+
+// ///Test Condition #6 ///
+
+console.log("Is car > 'Cultus'? I predict True.")
+console.log(car > 'Cultus')
+
+// ///Test Condition #7 ///
+
+console.log("Is car <= 'Cultus'? I predict True.")
+console.log(car <= 'Cultus')
+
+// ///Test Condition #8 ///
+
+console.log("Is car >= 'Cultus'? I predict True.")
+console.log(car >= 'Cultus')
+
+// ///Test Condition #9 ///
+
+console.log("Is car? I predict True.");
+console.log(car);
+
+// ///Test Condition #10 ///
+
+console.log("Is !car? I predict True.");
+console.log(!car);
+
+
+
+ 
