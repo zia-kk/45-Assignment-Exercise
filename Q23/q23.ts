22eadd00f7a18f74a8e5aa12ec56294132512843
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-19 18:07:51 +0500

    Exercise Q23 update

--- Q23/q23.ts
+++ Q23/q23.ts
@@ -1,53 +1,53 @@
-let car:string= "Cultus"
-// //Test Condition #1 /////
+let car:string= "subaru"
+// // //Test Condition #1 /////
 
-console.log("Is car == 'Cultus'? I predict True.")
-console.log(car == 'Cultus')
+console.log("Is car == 'subaru'? I predict True.")////true
+console.log(car == 'subaru')
 
-// //Test Condition #2/////
+// // //Test Condition #2/////
 
-console.log("Is car === 'Cultus'? I predict True.")
-console.log(car === 'Cultus')
+console.log("Is car !== 'subaru'? I predict True.")////false
+console.log(car !== 'subaru')
 
-// //Test Condition #3/////
+// // //Test Condition #3/////
 
-console.log("Is car != 'Cultus'? I predict True.")
-console.log(car != 'Cultus')
+console.log("Is car == 'Mehran'? I predict True.")/////false
+console.log(car == 'Mehran')
 
-// ///Test Condition #4///
+// // ///Test Condition #4///
 
-console.log("Is car !== 'Cultus'? I predict True.")
-console.log(car !== 'Cultus')
+console.log("Is car !== 'Mehran'? I predict True.")//////true
+console.log(car !== 'Mehran')
 
-// ///Test Condition #5 ///
+// // ///Test Condition #5 ///
 
-console.log("Is car < 'Cultus'? I predict True.")
-console.log(car < 'Cultus')
+console.log("Is car === 'BMW'? I predict True.")////false
+console.log(car == 'BMW')
 
-// ///Test Condition #6 ///
+// // ///Test Condition #6 ///
 
-console.log("Is car > 'Cultus'? I predict True.")
-console.log(car > 'Cultus')
+console.log("Is car !== 'BMW'? I predict True.")///true
+console.log(car !== 'BMW')
 
-// ///Test Condition #7 ///
+// // ///Test Condition #7 ///
 
-console.log("Is car <= 'Cultus'? I predict True.")
-console.log(car <= 'Cultus')
+console.log("Is car === 'ferrari'? I predict True.")////false
+console.log(car === 'ferrari')
 
-// ///Test Condition #8 ///
+// // ///Test Condition #8 ///
 
-console.log("Is car >= 'Cultus'? I predict True.")
-console.log(car >= 'Cultus')
+console.log("Is car !== 'ferrari'? I predict True.")/////true
+console.log(car !== 'ferrari')
 
-// ///Test Condition #9 ///
+// // ///Test Condition #9 ///
 
-console.log("Is car? I predict True.");
-console.log(car);
+console.log("Is car === 'Cuore'? I predict True.")////false
+console.log(car === 'Cuore')
 
-// ///Test Condition #10 ///
+// // ///Test Condition #10 ///
 
-console.log("Is !car? I predict True.");
-console.log(!car);
+console.log("Is car !== 'Cuore'? I predict True.")///true
+console.log(car !== 'Cuore')
 
 
 
