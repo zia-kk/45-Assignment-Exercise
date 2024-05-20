ea1bcba9269c394b55144c43ab56a71ce5ce66c5
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-20 19:39:40 +0500

    Exercise Q26

---
+++ Q26/q26.js
@@ -0,0 +1,18 @@
+"use strict";
+//1st Version
+Object.defineProperty(exports, "__esModule", { value: true });
+// let alien_color="green"
+// if(alien_color==="green"){
+//     console.log("Alien Color is green player just earned 5 points for shooting the alien.");
+// }
+// else{
+//         console.log("Alien color is not green.player earned 10 points");
+// }
+//2nd Version
+let alien_color = "Yellow";
+if (alien_color === "green") {
+    console.log("Alien Color is green player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Alien color is not green.player earned 10 points");
+}
---
+++ Q26/q26.ts
@@ -0,0 +1,25 @@
+
+//1st Version
+
+
+// let alien_color="green"
+// if(alien_color==="green"){
+//     console.log("Alien Color is green player just earned 5 points for shooting the alien.");
+    
+// }
+// else{
+//         console.log("Alien color is not green.player earned 10 points");
+        
+// }
+
+//2nd Version
+
+let alien_color="Yellow"
+if(alien_color==="green"){
+    console.log("Alien Color is green player just earned 5 points for shooting the alien.");
+    
+}
+else{
+        console.log("Alien color is not green.player earned 10 points");
+
+}
