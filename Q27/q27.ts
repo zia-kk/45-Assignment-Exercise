483398b3b5031281f80e8659c3833996838400cc
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-23 23:20:53 +0500

    Exercise Q27

---
+++ Q27/q27.ts
@@ -0,0 +1,46 @@
+// 1st version
+// let alien_color = "green";
+// if (alien_color === "green") {
+//   console.log("(Version 1st) Alien color is green,player earned 5 points.");
+// } 
+
+// else if (alien_color === "yellow") {
+//   console.log("(Version 1st) Alien color is yellow,player earned 10 points.");
+// } 
+
+// else if (alien_color === "red") {
+//   console.log("(Version 1st) Alien color is red,player earned 15 points.");
+// }
+
+
+// 2nd version
+
+// let alien_color = "yellow";
+// if (alien_color === "green") {
+//   console.log("(Version 2nd) Alien color is green,player earned 5 points.");
+// } 
+
+// else if (alien_color === "yellow") {
+//   console.log("(Version 2nd) Alien color is yellow,player earned 10 points.");
+// } 
+
+// else if (alien_color === "red") {
+//   console.log("(Version 2nd) Alien color is red,player earned 15 points.");
+// }
+
+// 3rd version
+
+let alien_color = "red";
+if (alien_color === "green") {
+  console.log("(Version 3rd) Alien color is green,player earned 5 points.");
+} 
+
+else if (alien_color === "yellow") {
+  console.log("(Version 3rd) Alien color is yellow,player earned 10 points.");
+} 
+
+else if (alien_color === "red") {
+  console.log("(Version 3rd) Alien color is red,player earned 15 points.");
+}
+
+
