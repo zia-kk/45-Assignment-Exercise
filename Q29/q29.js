5b4df806a000da8f8bf21096b08248ee1196321d
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-26 12:30:24 +0500

    Exercise Q29

---
+++ Q29/q29.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fav_friut = ["Mango", "Orange", "chiku"];
+if (fav_friut.includes("Mango")) {
+    console.log("I like Mango fruit");
+}
+;
+if (fav_friut.includes("Orange")) {
+    console.log("I like Orange fruit");
+}
+;
+if (fav_friut.includes("Apple")) {
+    console.log("I like Apple fruit");
+}
+if (fav_friut.includes("chiku")) {
+    console.log("I like chiku fruit");
+}
+if (fav_friut.includes("Banana")) {
+    console.log("I like Banana fruit");
+}
