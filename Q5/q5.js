33d715612501ba49f64989ce06231b875aa35770
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 20:00:05 +0500

    EXERCISE Q33

---
+++ Q5/q5.js
@@ -0,0 +1,5 @@
+"use strict";
+let qoute = "“A person who never made a mistake never tried anything new.”";
+let famous_Person = "Albert Einstein";
+let message = `${famous_Person} once said, ${qoute}`;
+console.log(message);
