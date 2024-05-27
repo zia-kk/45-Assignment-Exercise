50d2ca665bfb6f29f9d58ff681d22895d22c4951
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 21:28:02 +0500

    EXERCISE Q37

---
+++ Q37/q37.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size = "large", message = "I Love Typescript") {
+    console.log(`You order shirt size is: ${size} The message printed on it: ${message}`);
+}
+;
+make_shirt();
+make_shirt("Mediume");
+make_shirt("Small", "Love play Game");
---
+++ Q37/q37.ts
@@ -0,0 +1,12 @@
+
+
+function make_shirt(size:string="large" , message:string="I Love Typescript"){
+      console.log(`You order shirt size is: ${size} The message printed on it: ${message}`);
+      
+};
+
+
+make_shirt()
+make_shirt("Mediume")
+make_shirt("Small","Love play Game")
+
