b14f1e797bfdb5968f5ef6c3640928d9e913b87a
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 21:27:33 +0500

    EXERCISE Q36

---
+++ Q36/q36.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, message) {
+    console.log(`The shirt size is ${size} and it has the message: ${message}`);
+}
+;
+make_shirt("Large", "Hellow World");
+make_shirt("Small", "Love Typescript");
---
+++ Q36/q36.ts
@@ -0,0 +1,7 @@
+function make_shirt(size:string , message:string){
+       console.log(`The shirt size is ${size} and it has the message: ${message}`);
+
+};
+
+make_shirt("Large" , "Hellow World")
+make_shirt("Small" , "Love Typescript")
