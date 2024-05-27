ad9897f3e6feefc0d4bdacd99b002598b2e74b57
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 19:58:03 +0500

    EXERCISE Q34

---
+++ Q34/q34.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let fav_pizza = ["Meat pizza", "Margherita pizza", "Buffalo pizza"];
+for (let pizza of fav_pizza) {
+    console.log(pizza);
+}
+console.log("\n");
+for (let pizza of fav_pizza) {
+    console.log(`I realy like ${pizza}!`);
+}
+console.log("\n i realy like to eat pizza");
---
+++ Q34/q34.ts
@@ -0,0 +1,17 @@
+let fav_pizza:string[]=["Meat pizza" , "Margherita pizza" ,"Buffalo pizza"]
+
+for(let pizza of fav_pizza){
+    console.log(pizza);
+    
+}
+
+console.log("\n");
+
+for(let pizza of fav_pizza){
+    console.log(`I realy like ${pizza}!`);
+    
+}
+
+console.log("\n i realy like to eat pizza");
+
+ 
