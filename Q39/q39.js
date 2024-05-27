f876a509c66804c39d1619d8fcfce4192847ee3b
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 21:29:05 +0500

    EXERCISE Q39

---
+++ Q39/q39.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function city_country(city, country) {
+    return `${city} , ${country}`;
+}
+;
+let city1 = city_country("Karachi", "Pakistan");
+let city2 = city_country("Dehli", "India");
+let city3 = city_country("Manila", "Philipine");
+console.log(city1);
+console.log(city2);
+console.log(city3);
---
+++ Q39/q39.ts
@@ -0,0 +1,14 @@
+function city_country(city:string , country:string){
+  return `${city} , ${country}`
+     
+};
+
+let city1=city_country("Karachi" , "Pakistan")
+let city2=city_country("Dehli" , "India")
+let city3=city_country("Manila" , "Philipine")
+
+console.log(city1);
+console.log(city2);
+console.log(city3);
+
+
