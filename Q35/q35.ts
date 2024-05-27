4054830c4e7d3c1a7ef777aace286023b3533ea3
Zia Ur Rehman <pkzia112@gmail.com>
2024-05-27 19:58:34 +0500

    EXERCISE Q35

---
+++ Q35/q35.ts
@@ -0,0 +1,18 @@
+let animals:string[]=["Cow" , "Lion" ,"Cat"]
+
+for( let animal of animals){
+
+    console.log(animal);
+    
+}
+
+console.log("\n")
+
+for(let animal of animals){
+    console.log(`A ${animal} has a tail`);
+    
+}
+
+console.log("\nThese animals would make a great pet!");
+
+
